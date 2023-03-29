fb268c0f887f64f3ad0251c691bf33d603aafa3e
MattanCohen <mc.3552883@gmail.com>
2023-03-29 13:06:06 +0300

    setting up mattans scene to contain auto gen of a map
    this is the first version many rundowns needed

---
+++ Assets/Mattan/Scripts/generate.cs
@@ -0,0 +1,92 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class generate : MonoBehaviour
+{
+    
+    [SerializeField] bool diffHeights;
+    [SerializeField] int mapRadius = 3;
+    [SerializeField] float pBadTile = 0.2f;
+
+    [SerializeField] GameObject goodTile;
+    [SerializeField] GameObject dmgTile;
+    [SerializeField] GameObject windingTile;
+    
+    float heightDeltas = 3.5f;
+ 
+    void SpawnSurface(bool diffHeights){
+        int i=0, j=0;
+        float x, y;
+        for (i = -mapRadius + 1; i < mapRadius; i++)
+        {
+            for (j = -mapRadius + 1; j < mapRadius; j++)
+            {
+                if (i + j < mapRadius && i + j > -mapRadius)
+                {
+                    x = 1.75f * i + (1.75f / 2.0f) * j;
+                    y = 1.5f * j;
+
+                    float z = !diffHeights ? 0 : Mathf.Sin((i + j) * 2.2f) / heightDeltas;
+                    bool spawnGoodTile = Random.Range(0f,1f) > pBadTile || ((Mathf.Abs(i) < 2) && (Mathf.Abs(j) < 2));
+                    GameObject newTile = Instantiate(
+                                            spawnGoodTile ? goodTile : (Random.Range(0f,1f) < 0.5f ? dmgTile : windingTile), 
+                                            new Vector3(x, z, y), 
+                                            Quaternion.Euler(0, 0, 0));
+                    newTile.transform.parent = this.transform;
+                }
+
+            }
+        }
+    }
+    
+    void Start()
+    {  
+        SpawnSurface(diffHeights);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    class Polynom
+    {
+        public float a, b, c, d;
+        public Polynom(float a, float b, float c, float d)
+        {
+            this.a = a; this.b = b; this.c = c; this.d = d;
+        }
+
+        public float calculate(float x)
+        {
+            a = a * x * x * x;
+            b = a * x * x;
+            c = a * x;
+            d = a;
+            return (a + b + c + d);
+        }
+    }
+/*
+    class tile
+    {
+        public int x;
+        public int y;
+        public int z=0;
+
+        public tile(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        
+        public void setX(int x) { this.x = x; }
+        public void setY(int y) { this.y = y; }
+        public void setZ(int z) { this.z = z; }
+        
+    }
+*/
+
+}
