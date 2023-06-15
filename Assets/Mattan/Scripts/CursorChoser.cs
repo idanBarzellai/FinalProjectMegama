ecb140df1c8405a280251fdd940c1f775df070f3
MattanCohen <mc.3552883@gmail.com>
2023-06-15 03:08:50 +0300

    custom cursors

--- Assets/Mattan/Scripts/CursorChoser.cs
+++ Assets/Mattan/Scripts/CursorChoser.cs
@@ -6,16 +6,20 @@ using UnityEngine.SceneManagement;
 public class CursorChoser : MonoBehaviour
 {
     public static CursorChoser instance;
+    [SerializeField] Texture2D [] cursors;
     void Start()
     {
         if (instance != null) Destroy(gameObject);
         instance = this;
 
         DontDestroyOnLoad(gameObject);       
-             
+
         PickNewCursor();
     }
     private void OnSceneLoaded(Scene scene, LoadSceneMode mode){PickNewCursor();}
 
-    void PickNewCursor(){}
+    void PickNewCursor()
+        {if (cursors.Length > 0)
+            Cursor.SetCursor(cursors[Random.Range(0, cursors.Length)], Vector2.zero, CursorMode.Auto);}
+
 }
